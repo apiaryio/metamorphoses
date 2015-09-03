68e6e9de41d334dee7f09737010dd3e47b15ce42
XVincentX <vincenz.chianese@yahoo.it>
2015-09-03 14:00:10 +0200

    Setting up browser integration

--- karma.conf.js
+++ karma.conf.js
@@ -27,9 +27,13 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.coffee': ['coffee', 'browserify']
+      '**/*.coffee': ['browserify']
     },
 
+    browserify: {
+      transform: ['coffeeify'],
+      extensions: ['.coffee']
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
